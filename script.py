c4c9e3665322af4f9bf26b971e325d6364cab2bc
Rud <r.hildermann@outlook.de>
2025-08-28 11:24:40 +0200

    added menu

--- script.py
+++ script.py
@@ -4,7 +4,6 @@
 # NUMBER GUESSING GAME
 import random
 
-feature/guessing_number
 def number_guessing_game():
     random_number = random.randint(1, 100)
     attempts = 0 
@@ -36,18 +35,17 @@ def number_guessing_game():
             print('Not a number, please input a number')
 
 
-number_guessing_game()
-
 # PAPER, ROCK, SCISSORS
 import random
 
-print("<-------ROCK------PAPER------SCISSORS------GAME------>")
-print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
-      + "Rock vs Paper -> Paper wins \n"
-      + "Rock vs Scissors -> Rock wins \n"
-      + "Paper vs Scissors -> Scissors wins \n")
 
 def rockpaperscissors():
+    print("<-------ROCK------PAPER------SCISSORS------GAME------>")
+    print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
+        + "Rock vs Paper -> Paper wins \n"
+        + "Rock vs Scissors -> Rock wins \n"
+        + "Paper vs Scissors -> Scissors wins \n")
+    
     while True:
         print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
 
@@ -117,15 +115,17 @@ def rockpaperscissors():
         if ans == "n":
             break
         ans
-rockpaperscissors()
+
+
 # After coming out of the while loop, print thanks for playing
-print()
-print("(<== Thanks for playing ==>)")
-print("See you Again!")
-print("<------------THE END-------------->")
+    print()
+    print("(<== Thanks for playing ==>)")
+    print("See you Again!")
+    print("<------------THE END-------------->")
 
 
 # TIC-TAC-TOE
+    
 # Function to print the board
 
 
@@ -135,12 +135,54 @@ print("<------------THE END-------------->")
 
 
 # Function to run fo the actual tic-tac-toe game where your other functions will be used
-
+def tic_tac_toe():
+    print("CURRENTLY UNDER DEVELOPMENT...")
    
 
 # MENU SYSTEM
+def main_menu():
+    try:
+        # MAIN LOOP
+        global user_exit
+        user_exit = False
+        while not user_exit:
+            user_input = input("\n...:::| WELCOME TO THE ARCADE MINI GAME COLLECTION! |:::...\n\nWhich game do you want to play? \n1. Number Guessing \n2. Rock,Paper,Scissors \n3. Tic-Tac-Toe \n4. EXIT\n \nEnter your number: ")
+            if user_input.isdigit():
+                print("")
+                print("")
+                    #print("Input is digit")
+                if int(user_input) in range(1, 5):
+                    #print("Input in range")
+                    if user_input == "1":
+                        print("...:::| NUMBER GUESSING GAME |:::...\n")
+                        number_guessing_game() # Play Number Guessing 
+                    if user_input == "2":
+                        print("...:::| ROCK, PAPER, SCISSORS |:::...\n")
+                        rockpaperscissors() # Play Rock, Paper, Scissors 
+                    if user_input == "3":
+                        print("...:::| TIC-TAC-TOE |:::...\n")
+                        tic_tac_toe()# Play Tic-Tac-Toe (UNDER DEVELOPMENT)
+                    if user_input == "4":
+                        exit() # Exit
+                else:
+                    print("Only numbers between 1-4 allowed!")
+            else:
+                print("Only numbers allowed!")
+    except KeyboardInterrupt:
+        ...
 
-    
+def exit():
+    while True:
+        exit_input = input("\nAre you sure, you want to exit? [Y]ES/[N]O: ").strip().lower()
+        if exit_input in ["n", "no"]:
+            return
+        if exit_input in ["y", "yes"]:
+            print("\n...:::| THANKS FOR PLAYING, GOODBYE! |:::...\n")
+            global user_exit
+            user_exit = True
+            return
+        else:
+            print("Incorrect input. Please answer with '[Y]ES' or '[N]O'.")
 
-    
 # RUN THE MENU
+main_menu()    
