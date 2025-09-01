8d32f8a220ea9e1a483f24ae89ef42ceb040750b
Nedim-34 <nedim1demirel@gmail.com>
2025-09-01 15:27:58 +0200

    finalsied my own part

--- script.py
+++ script.py
@@ -21,57 +21,52 @@
 # Function to check the winner
 
 
-
-# Function to run fo the actual tic-tac-toe game where your other functions will be used
-
-def tic_tac_toe():
-    board = [' '] * 9
-    current_player = 'X'
-    turns = 0
-
-    print(" Welcome to Tic Tac Toe Game!")
+    #####
     
-    while True:
-        print_board(board)
-        
-        # Player Input
-        try:
-            move = int(input(f"Player '{current_player}', choose a position (1-9): ")) - 1
-            if move < 0 or move > 8:
-                print("Invalid input. Please enter a number between 1 and 9.")
-                continue
-            if board[move] != ' ':
-                print("Position already taken. Try again.")
-                continue
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-        
 
-        # execute a move
-        board[move] = current_player
-        turns += 1
-        
-        # Switch player
-        current_player = 'O' if current_player == 'X' else 'X'
     
-    # Ask if the user wants to play again
+    def get_player_move(board, player):
+        """Gets valid player input and returns the position."""
+        while True:
+            try:
+                move = int(input(f"Player {player}, choose a position (1-9): "))
+                if 1 <= move <= 9:
+                    if board[move - 1] == " ":
+                        return move - 1
+                    else:
+                        print("Position already taken. Try again.")
+                else:
+                    print("Invalid input. Please enter a number between 1 and 9.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    def play_game():
+        """Main function to run the Tic-Tac-Toe game."""
+        board = [" "] * 9  # Represents the 9 positions on the board
+        current_player = "X"
+        game_over = False
+        moves_count = 0
+
+        #####
+        
+                # Switch players
+                current_player = "O" if current_player == "X" else "X"
+
+        # Ask if the user wants to play again
         print(" ")
         while True:
             print("Are you ready to play again...! (Y/N)")
             ans = input().lower()
             if ans == "y":
-                tic_tac_toe()
+                play_game()
                 break
             elif ans == "n":
                 print("Thanks for playing!")
                 break
             else:
                 print("Invalid input! Please enter Y or N")
+    play_game()
 
-if __name__ == "__main__":
-    tic_tac_toe()
-   
 
 # MENU SYSTEM
 
